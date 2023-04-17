f6809ae4c137d1bdb66c3ca45d7522bb768a907f
cuongqn235 <49529190+cuongqn235@users.noreply.github.com>
2023-04-17 23:19:31 +0700

    config firebase

--- lib/inject_dependency/inject_dependency.config.dart
+++ lib/inject_dependency/inject_dependency.config.dart
@@ -10,15 +10,16 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:dio/dio.dart' as _i3;
-import 'package:flutter_secure_storage/flutter_secure_storage.dart' as _i6;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart' as _i7;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
+import '../base/datasource/api_service.dart' as _i4;
 import '../base/datasource/local/auth/authentication_local_datasource.dart'
-    as _i5;
+    as _i6;
 import '../base/datasource/local/auth/i_authentication_local_datasource.dart'
-    as _i4;
-import '../base/module.dart' as _i7;
+    as _i5;
+import '../base/module.dart' as _i8;
 
 extension GetItInjectableX on _i1.GetIt {
   // initializes the registration of main-scope dependencies inside of GetIt
@@ -36,8 +37,12 @@ extension GetItInjectableX on _i1.GetIt {
       () => networkModule.dioDefault,
       instanceName: 'defaultDio',
     );
-    gh.lazySingleton<_i4.IAuthenticationLocalDataSource>(() =>
-        _i5.AuthenticationLocalDataSource(gh<_i6.FlutterSecureStorage>()));
+    gh.factory<_i4.IApiService>(
+      () => _i4.DefaultApiService(gh<_i3.Dio>(instanceName: 'defaultDio')),
+      instanceName: 'DefaultApiService',
+    );
+    gh.lazySingleton<_i5.IAuthenticationLocalDataSource>(() =>
+        _i6.AuthenticationLocalDataSource(gh<_i7.FlutterSecureStorage>()));
     gh.factory<String>(
       () => networkModule.baseUrl,
       instanceName: 'baseUrl',
@@ -46,4 +51,4 @@ extension GetItInjectableX on _i1.GetIt {
   }
 }
 
-class _$NetworkModule extends _i7.NetworkModule {}
+class _$NetworkModule extends _i8.NetworkModule {}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_core
+import firebase_storage
 import flutter_secure_storage_macos
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
